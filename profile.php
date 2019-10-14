cacf09437fa9c89fcbb36c2cca55b0354d4c28ff
vangp42 <50687664+vangp42@users.noreply.github.com>
2019-10-14 13:55:11 -0500

    Add profile.php

---
+++ profile.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include 'controller.php';
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <title>Profile - Friendly Cuisine</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="keywords" content="Friendly Cuisine">
+    <?php include 'resources.php'; ?>
+</head>
+
+<header>
+    <?php include 'header.php'; ?>
+</header>
+
+<body>
+    <h1>Profile</h1>
+    
+<!--    <img src="img/default-profile-pic.png">-->
+    <img id="profileImage" src="img/default-profile-pic.png">
+
+</body>
